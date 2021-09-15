1ef10bb653b191f41cfd8a2bba324a6499f81d66
Maros Korinek <maros.korinek@skyscanner.net>
2021-09-15 20:34:44 +0100

    Extract constants to config

---
+++ src/main/db/data_access/config.py
@@ -0,0 +1,10 @@
+# tables
+USERS_TABLE = "users"
+
+# field names
+DB_ID_FIELD = "id"
+DB_EMAIL_FIELD = "email"
+
+# indexes
+USER_BY_EMAIL_INDEX = "users-by-email-index"
+USERS_BY_ID_INDEX = "users-by-id-index"
--- src/main/db/data_access/user.py
+++ src/main/db/data_access/user.py
@@ -1,20 +1,24 @@
+from db.data_access.config import (
+    DB_EMAIL_FIELD,
+    USERS_BY_ID_INDEX,
+    USERS_TABLE,
+    USER_BY_EMAIL_INDEX,
+)
 from db.data_access.mappers.user_mapper import user_to_dict, dict_to_user
 from db.db_client import DbClient
 from model.user import User
 
-TABLE = 'users'
-
 
 def put_user(db: DbClient, user: User):
-    db.put_item(TABLE, user_to_dict(user))
+    db.put_item(USERS_TABLE, user_to_dict(user))
 
 
 def get_user_by_email(db: DbClient, email: str) -> User:
     # todo extract to config, maybe - `UsersTableConfig
-    result = db.query_index(TABLE, 'users-by-email-index', 'email', email)
+    result = db.query_index(USERS_TABLE, USER_BY_EMAIL_INDEX, DB_EMAIL_FIELD, email)
     return dict_to_user(result[0]) if result else None
 
 
 def get_user_by_id(db: DbClient, id: str) -> User:
-    result = db.query_index(TABLE, 'users-by-id-index', 'id', id)
+    result = db.query_index(USERS_TABLE, USERS_BY_ID_INDEX, DB_EMAIL_FIELD, id)
     return dict_to_user(result[0]) if result else None
